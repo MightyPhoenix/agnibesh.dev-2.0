df4cf14b218ed41e80417a9402f84c64bd5370d8
Agnibesh Mukherjee <agnibesh00mukherjee@gmail.com>
2021-09-28 23:33:06 +0530

    button stable

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   AiFillLinkedin,
   AiOutlineTwitter,
 } from "react-icons/ai";
+import { FaPlay, FaPause } from "react-icons/fa";
 import { FaSpotify } from "react-icons/fa";
 import Fade from "react-reveal/Fade";
 
@@ -38,81 +39,85 @@ function App() {
   );
 
   return (
-    <div className="App">
-      <button onClick={toggle}>{playing ? "Pause" : "Play"}</button>
-      <Halo className="hero">
-        <Fade bottom duration={3000}>
-          <div>
-            <h1>Agnibesh Mukherjee</h1>
-            <p>a javascript 🥷💖 </p>
-          </div>
-        </Fade>
-      </Halo>
-      <section className="spacer spacer1">
-        <div className="grid1">
-          <div></div>
-          <div className="wassup">
-            <h1>👋 Wassup?</h1>
-            <Fade bottom duration={2000}>
-              <div className="grid1-1">
-                <div className="border-right-white">
-                  <ul>
-                    <li>✌🏻 B.Tech CSE 4th Year (pls hire me)</li>
-                    <li>💕 Currently Interning at Glowradius.com</li>
-                    <li>😱 Previously Newslaundry.com</li>
-                  </ul>
+    <>
+      <button className="theButton" onClick={toggle}>
+        {playing ? <FaPause /> : <FaPlay />}
+      </button>
+      <div className="App">
+        <Halo className="hero">
+          <Fade bottom duration={3000}>
+            <div>
+              <h1>Agnibesh Mukherjee</h1>
+              <p>a javascript 🥷💖 </p>
+            </div>
+          </Fade>
+        </Halo>
+        <section className="spacer spacer1">
+          <div className="grid1">
+            <div></div>
+            <div className="wassup">
+              <h1>👋 Wassup?</h1>
+              <Fade bottom duration={2000}>
+                <div className="grid1-1">
+                  <div className="border-right-white">
+                    <ul>
+                      <li>✌🏻 B.Tech CSE 4th Year (pls hire me)</li>
+                      <li>💕 Currently Interning at Glowradius.com</li>
+                      <li>😱 Previously Newslaundry.com</li>
+                    </ul>
+                  </div>
+                  <div className="border-left-white">
+                    <ul>
+                      <li>🤑 Winner, ACM East India Hackathon 2020</li>
+                      <li>🧐 Co-Authored a Paper KIIT ICDCIT PIC 2020</li>
+                      <li>🎹 Makes music and take photos 📸</li>
+                    </ul>
+                  </div>
                 </div>
-                <div className="border-left-white">
-                  <ul>
-                    <li>🤑 Winner, ACM East India Hackathon 2020</li>
-                    <li>🧐 Co-Authored a Paper KIIT ICDCIT PIC 2020</li>
-                    <li>🎹 Makes music and take photos 📸</li>
-                  </ul>
-                </div>
-              </div>
-            </Fade>
+              </Fade>
+            </div>
           </div>
-        </div>
-      </section>
-      <section className="spacer spacer2 d-flex-vertical-between">
-        <div className="grid2">
-          <div className="wassup2">
-            <Fade top>
-              <h1>
-                👉👈 Contact <strike>Us</strike> Me? 😳
-              </h1>
-            </Fade>
-            <Fade bottom>
-              <IconContext.Provider value={{ className: "icons" }}>
-                <div className="grid2-1">
-                  <a target="_blank" href="#">
-                    <AiFillGithub />
-                  </a>
-                  <a target="_blank" href="#">
-                    <AiOutlineInstagram />
-                  </a>
-                  <a target="_blank" href="#">
-                    <FaSpotify />
-                  </a>
-                  <a target="_blank" href="#">
-                    <AiFillLinkedin />
-                  </a>
-                  <a target="_blank" href="#">
-                    <AiOutlineTwitter />
-                  </a>
-                </div>
-              </IconContext.Provider>
-            </Fade>
+        </section>
+        <section className="spacer spacer2 d-flex-vertical-between">
+          <div className="grid2">
+            <div className="wassup2">
+              <Fade top>
+                <h1>
+                  👉👈 Contact <strike>Us</strike> Me? 😳
+                </h1>
+              </Fade>
+              <Fade bottom>
+                <IconContext.Provider value={{ className: "icons" }}>
+                  <div className="grid2-1">
+                    <a target="_blank" href="#">
+                      <AiFillGithub />
+                    </a>
+                    <a target="_blank" href="#">
+                      <AiOutlineInstagram />
+                    </a>
+                    <a target="_blank" href="#">
+                      <FaSpotify />
+                    </a>
+                    <a target="_blank" href="#">
+                      <AiFillLinkedin />
+                    </a>
+                    <a target="_blank" href="#">
+                      <AiOutlineTwitter />
+                    </a>
+                  </div>
+                </IconContext.Provider>
+              </Fade>
+            </div>
+            <div className="footer">
+              <h3>
+                Made with <span className="animate-beat">❤️</span> by Agnibesh ©
+                2021
+              </h3>
+            </div>
           </div>
-          <div className="footer">
-            <h3>
-              Made with <span className="animate-beat">❤️</span> by Agnibesh ©
-              2021
-            </h3>
-          </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </>
   );
 }
 
